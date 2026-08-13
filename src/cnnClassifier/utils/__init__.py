edd09fde80020e8ffcf80a8238dc1de88830bd5d
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-14 02:36:17 +0600

    hjbjk

---
+++ src/cnnClassifier/utils/__init__.py
@@ -0,0 +1,17 @@
+import sys 
+import os 
+import logging 
+
+logging_stream = logging.StreamHandler(sys.stdout) 
+
+log_dir = "logs" 
+log_filepath = os.path.join(log_dir, "running_logs.log") 
+os.makedirs(log_dir, exist_ok=True) 
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[logging_stream, logging.FileHandler(log_filepath, mode="a")],
+    format="[%(asctime)s]: %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logger=logging.getlogger(__name__)
