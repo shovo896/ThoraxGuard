20389e73b0105e117fe065e9a653f928831b429a
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-15 11:01:15 +0600

    joiji

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
+import os 
+
 
 from cnnClassifier.utils.common import create_directories, read_yaml
 
